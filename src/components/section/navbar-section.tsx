60768eff59aa7f7b000794da7d571abf621f66ce
mamsul <imamsulthoni.amd@gmail.com>
2023-12-04 09:58:13 +0700

    fix: add li tag inside ul

--- src/components/section/navbar-section.tsx
+++ src/components/section/navbar-section.tsx
@@ -1,35 +1,40 @@
-import { BookText, PackageOpen, TerminalSquare } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+import { BookText, PackageOpen, TerminalSquare } from 'lucide-react';
+import Link from 'next/link';
 
 const NavbarSection = () => {
   return (
     <nav className="screen-size mt-4">
-      <ul className="w-full flex flex-wrap gap-6 text-dark-text-primary">
-        <Link
-          href="/"
-          className="font-light tracking-wider text-sm border-b border-dotted border-dark-text-secondary pb-0.5"
-        >
-          <span className="inline-flex items-center gap-1.5">
-            <BookText className="w-3 h-3 md:w-4 md:h-4 text-gray-500" /> Tentang
-          </span>
-        </Link>
-        <Link
-          href="/portfolio"
-          className="font-light tracking-wider text-sm border-b border-dotted border-dark-text-secondary pb-0.5"
-        >
-          <span className="inline-flex items-center gap-1.5">
-            <PackageOpen className="w-3 h-3 md:w-4 md:h-4 text-gray-500" /> Portofolio
-          </span>
-        </Link>
-        <Link
-          href="/playground"
-          className="font-light tracking-wider text-sm border-b border-dotted border-dark-text-secondary pb-0.5"
-        >
-          <span className="inline-flex items-center gap-1.5">
-            <TerminalSquare className="w-3 h-3 md:w-4 md:h-4 text-gray-500" /> Playground
-          </span>
-        </Link>
+      <ul className="flex w-full flex-wrap gap-6 text-dark-text-primary">
+        <li>
+          <Link
+            href="/"
+            className="border-b border-dotted border-dark-text-secondary pb-0.5 text-sm font-light tracking-wider">
+            <span className="inline-flex items-center gap-1.5">
+              <BookText className="h-3 w-3 text-gray-500 md:h-4 md:w-4" />{' '}
+              Tentang
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/portfolio"
+            className="border-b border-dotted border-dark-text-secondary pb-0.5 text-sm font-light tracking-wider">
+            <span className="inline-flex items-center gap-1.5">
+              <PackageOpen className="h-3 w-3 text-gray-500 md:h-4 md:w-4" />{' '}
+              Portofolio
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/playground"
+            className="border-b border-dotted border-dark-text-secondary pb-0.5 text-sm font-light tracking-wider">
+            <span className="inline-flex items-center gap-1.5">
+              <TerminalSquare className="h-3 w-3 text-gray-500 md:h-4 md:w-4" />{' '}
+              Playground
+            </span>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
